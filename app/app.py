01ce933da27bb152b3800235bfe54a1c856de476
Mayuri Gunjal <mayuri.gunjal.03@gmail.com>
2026-06-20 10:59:26 +0530

    Added Streamlit app

---
+++ app/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(page_title="Customer Engagement Retention Analytics")
+
+st.title("Customer Engagement & Retention Analytics")
+
+st.header("Project Overview")
+
+st.write("""
+This project analyzes customer engagement,
+product utilization, and retention behavior
+using banking customer data.
+""")
+
+st.subheader("Objectives")
+
+st.write("- Identify churn patterns")
+st.write("- Analyze customer engagement")
+st.write("- Evaluate product utilization")
+st.write("- Improve customer retention")
+
+st.subheader("Tools Used")
+
+st.write("Python, Tableau, Excel, Streamlit")
+
+st.success("Project Developed by Mayuri Gunjal")
